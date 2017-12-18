19350127b72d0649649a8a201dcf84c043e14842
Ash Levy <ashlea@protonmail.com>
2017-12-18 12:21:24 -0500

    move errors into their own file

---
+++ src/errors.rs
@@ -0,0 +1,49 @@
+// Create the Error, ErrorKind, ResultExt, and Result types
+#[derive(Debug, ErrorChain)]
+pub enum ErrorKind {
+    Msg(String),
+
+    #[error_chain(foreign)] FreeTypeError(::freetype::Error),
+
+    #[cfg(feature = "opengl")]
+    #[error_chain(foreign)]
+    GliumDisplayCreationError(::glium::backend::glutin::DisplayCreationError),
+    #[cfg(feature = "opengl")]
+    #[error_chain(foreign)]
+    GliumDrawError(::glium::DrawError),
+    #[cfg(feature = "opengl")]
+    #[error_chain(foreign)]
+    GliumGlutinCreationError(::glium::glutin::CreationError),
+    #[cfg(feature = "opengl")]
+    #[error_chain(foreign)]
+    GliumIncompatibleOpenGlError(::glium::IncompatibleOpenGl),
+    #[cfg(feature = "opengl")]
+    #[error_chain(foreign)]
+    GliumProgramChooserCreationError(::glium::program::ProgramChooserCreationError),
+    #[cfg(feature = "opengl")]
+    #[error_chain(foreign)]
+    GliumProgramCreationError(::glium::ProgramCreationError),
+    #[cfg(feature = "opengl")]
+    #[error_chain(foreign)]
+    GliumSwapBuffersError(::glium::SwapBuffersError),
+    #[cfg(feature = "opengl")]
+    #[error_chain(foreign)]
+    GliumTextureCreationError(::glium::texture::TextureCreationError),
+    #[cfg(feature = "opengl")]
+    #[error_chain(foreign)]
+    GliumVertexCreationError(::glium::vertex::BufferCreationError),
+
+    #[error_chain(foreign)] ImageError(::image::ImageError),
+
+    #[error_chain(foreign)] LogSetLoggerError(::log::SetLoggerError),
+
+    #[error_chain(foreign)] NFDError(::nfd::error::NFDError),
+
+    #[error_chain(foreign)] SerdeYamlError(::serde_yaml::Error),
+
+    #[error_chain(foreign)] StdIoError(::std::io::Error),
+    #[error_chain(foreign)] StdParseIntError(::std::num::ParseIntError),
+    #[error_chain(foreign)] StdParseFloatError(::std::num::ParseFloatError),
+
+    #[error_chain(foreign)] TimeParseError(::time::ParseError),
+}
--- src/main.rs
+++ src/main.rs
@@ -38,65 +38,14 @@ extern crate image;
 extern crate num_rational;
 
 pub mod config;
+pub mod errors;
 pub mod font;
 pub mod platform;
-pub mod util;
 pub mod source;
+pub mod util;
 
 #[cfg(feature = "opengl")]
 pub mod opengl;
-
-mod errors {
-    // Create the Error, ErrorKind, ResultExt, and Result types
-    #[derive(Debug, ErrorChain)]
-    pub enum ErrorKind {
-        Msg(String),
-
-        #[error_chain(foreign)] FreeTypeError(::freetype::Error),
-
-        #[cfg(feature = "opengl")]
-        #[error_chain(foreign)]
-        GliumDisplayCreationError(::glium::backend::glutin::DisplayCreationError),
-        #[cfg(feature = "opengl")]
-        #[error_chain(foreign)]
-        GliumDrawError(::glium::DrawError),
-        #[cfg(feature = "opengl")]
-        #[error_chain(foreign)]
-        GliumGlutinCreationError(::glium::glutin::CreationError),
-        #[cfg(feature = "opengl")]
-        #[error_chain(foreign)]
-        GliumIncompatibleOpenGlError(::glium::IncompatibleOpenGl),
-        #[cfg(feature = "opengl")]
-        #[error_chain(foreign)]
-        GliumProgramChooserCreationError(::glium::program::ProgramChooserCreationError),
-        #[cfg(feature = "opengl")]
-        #[error_chain(foreign)]
-        GliumProgramCreationError(::glium::ProgramCreationError),
-        #[cfg(feature = "opengl")]
-        #[error_chain(foreign)]
-        GliumSwapBuffersError(::glium::SwapBuffersError),
-        #[cfg(feature = "opengl")]
-        #[error_chain(foreign)]
-        GliumTextureCreationError(::glium::texture::TextureCreationError),
-        #[cfg(feature = "opengl")]
-        #[error_chain(foreign)]
-        GliumVertexCreationError(::glium::vertex::BufferCreationError),
-
-        #[error_chain(foreign)] ImageError(::image::ImageError),
-
-        #[error_chain(foreign)] LogSetLoggerError(::log::SetLoggerError),
-
-        #[error_chain(foreign)] NFDError(::nfd::error::NFDError),
-
-        #[error_chain(foreign)] SerdeYamlError(::serde_yaml::Error),
-
-        #[error_chain(foreign)] StdIoError(::std::io::Error),
-        #[error_chain(foreign)] StdParseIntError(::std::num::ParseIntError),
-        #[error_chain(foreign)] StdParseFloatError(::std::num::ParseFloatError),
-
-        #[error_chain(foreign)] TimeParseError(::time::ParseError),
-    }
-}
 
 #[cfg(unix)]
 use signal::Signal;
